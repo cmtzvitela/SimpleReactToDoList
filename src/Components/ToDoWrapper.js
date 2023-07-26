93f3c5bd58214e1a504794cb802341061f594695
cmtzvitela <89888862+cmtzvitela@users.noreply.github.com>
2023-07-26 16:58:22 -0600

    Add tasks and delete them

---
+++ src/Components/ToDoWrapper.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { ToDoForm } from './ToDoForm';
+import { v4 as uuidv4 } from 'uuid';
+import { ToDo } from './ToDo';
+uuidv4();
+
+export const ToDoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, { id: uuidv4(), task: todo, completed: false, isEditing: false }]);
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo)));
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  
+  return (
+    <div className="TodoWrapper">
+      <h1>Get Things Done!</h1>
+      <ToDoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <ToDo task={todo} key={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo} />
+      ))}
+    </div>
+  );
+};
